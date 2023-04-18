157fde70f760d5b460fcc76b9b953fddc29b0331
Max <max@aiwon.org>
2023-04-18 15:33:34 +0300

    started implementation and testing of access gated liverpeer functionality;

--- apps/next-ts/pages/index.tsx
+++ apps/next-ts/pages/index.tsx
@@ -4,18 +4,11 @@ import { ConnectButton } from "@rainbow-me/rainbowkit";
 import {CreateAndViewAsset} from "@/components/CreateAndViewAsset";
 
 export default function Home() {
-  const [count, setCount] = useAtom(countAtom);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CreateAndViewAsset/>
       {/*<Stream />*/}
       <ConnectButton />
-      <button
-        className="btn btn-lg btn-primary"
-        onClick={() => setCount((c) => c + 1)}
-      >
-        Button
-      </button>
     </main>
   );
 }
