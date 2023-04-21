742bd2fed2b5094ec2544268bce818c293581d28
Max <max@aiwon.org>
2023-04-21 16:11:15 +0300

    upd readme

--- apps/next-ts/pages/test.tsx
+++ apps/next-ts/pages/test.tsx
@@ -6,7 +6,6 @@ export default function Test() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <CreateAndViewAsset />
-      <Stream />
       <div>hi</div>
       <Motion1 />
       <div>hi</div>
